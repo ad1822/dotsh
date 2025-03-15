b31e32885e0e0ee9568a7fdfb3c335441ae88f2b
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-03-15 23:32:58 +0530

    Scripts for copy and name

---
+++ syncdots.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+files=("hypr" "waybar" "cava" "fastfetch" "gtk-3.0" "gtk-4.0" "hypridle" "kitty" "k9s" "rofi" "yazi" "starship")
+
+for dir in "${files[@]}"; do
+    echo "syncing with hyprdots $dir"
+    cp -ru ~/.config/$dir ~/hyprdots
+done
