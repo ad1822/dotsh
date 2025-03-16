33629b9f1c921667a3a2687c2c5c7a30f026893e
ayushDumasia <220170107033@gmail.com>
2025-03-16 22:05:55 +0530

    Some changes in sync All

--- syncAll.sh
+++ syncAll.sh
@@ -5,6 +5,11 @@ hyprdots=$HOME/SideProjects/Scripts/syncdots.sh
 echo "===> Syncing Hyprdots"
 bash $hyprdots
 
+repo=$HOME/SideProjects/Scripts/pushDots.sh
+
+echo "===> Pushing to Github"
+bash $repo
+
 cloud=$HOME/SideProjects/Scripts/rsync.sh
 
 echo "===> Syncing Clouds"
