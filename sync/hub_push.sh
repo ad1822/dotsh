0b095d4aec16cdcdabf3cce08ad3480a1cf70629
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-03-26 16:51:48 +0530

    Script for selecting wallpaper from yazi

---
+++ sync/hub_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+eval "$(ssh-agent -s)" > /dev/null
+ssh-add ~/.ssh/github
+
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: $0 <commit-message> <branch-name>"
+    exit 1
+fi
+
+git status
+git add .
+git commit -m "$1"
+git push origin "$2"
+
+echo "Changes pushed successfully to origin/$2."
