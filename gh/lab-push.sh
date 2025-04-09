6fcb924d07b56d9edccb92509d3fd2be90681eb5
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-04-09 23:18:36 +0530

    perf: Improve Github and Gitlab Push scripts

--- gh/lab-push.sh
+++ gh/lab-push.sh
@@ -2,18 +2,35 @@
 
 set -e
 
-eval "$(ssh-agent -s)" > /dev/null
-ssh-add ~/.ssh/gitlab
+cat <<'EOF'
+
+┌─┐┬┌┬┐┬ ┬┬ ┬┌┐
+│ ┬│ │ ├─┤│ │├┴┐
+└─┘┴ ┴ ┴ ┴└─┘└─┘
+
+EOF
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: $0 <commit-message> <branch-name>"
+    echo -e "Usage: \e[0;31m ❌ hub-push.sh <commit-message> <branch-name>\e[0m"
     exit 1
 fi
 
-git s5
-git status
+eval "$(ssh-agent -s)" > /dev/null
+OUTPUT=$(ssh-add ~/.ssh/gitlab 2>&1)
+
+if [[ $OUTPUT =~ Identity\ added:\ .*\((.*)\) ]]; then
+  EMAIL=${BASH_REMATCH[1]}
+  echo -e "\e[1;32m ✔ Identity added for:\e[0m \e[1;33m$EMAIL\e[0m"
+else
+  echo -e "\e[1;31m✖ Failed to add identity\e[0m"
+  echo -e "\e[0;90m$OUTPUT\e[0m"
+fi
+
+# git s5
+echo -e "\n\e[1;36m 📦 Git status:\e[0m"
+git ss
 git add .
 git commit -m "$1"
 git push origin "$2"
 
-echo "Changes pushed successfully to origin/$2."
+echo -e "\e[1;32m ✔ Changes pushed successfully to \e[1;34morigin/$2\e[0m."
