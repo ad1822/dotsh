86ad2d038b14a68c3c6f5c1cca24b5e1c302afba
Ayush Dumasia <ayushdumasia00@gmail.com>
2025-05-15 15:40:19 +0530

    fix 🐛: Sync dots script (-u not working)

--- sync/sync-dots.sh
+++ sync/sync-dots.sh
@@ -21,15 +21,15 @@ BLUE="\e[34m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-files=("hypr" "git" "waybar" "cava" "fastfetch" "gtk-3.0" "gtk-4.0" "hypridle" "kitty" "k9s" "rofi" "yazi" "starship" "dunst")
+files=("hypr" "git" "waybar" "cava" "fastfetch" "hypridle" "kitty" "k9s" "rofi" "yazi" "starship" "dunst")
 
 for dir in "${files[@]}"; do
     echo -e "${BLUE}=> Syncing with hyprdots ${YELLOW}${dir}${RESET}"
-    cp -ru ~/.config/$dir ~/hyprdots
+    cp -r ~/.config/$dir ~/hyprdots
 done
 
 echo -e "${BLUE}=> Syncing with hyprdots ${YELLOW}Local Scripts${RESET}"
-cp -ru "$HOME/.local/bin" ~/hyprdots
+cp -r "$HOME/.local/bin" ~/hyprdots
 
 echo -e "${BLUE}=> Syncing with hyprdots ${YELLOW}.zshrc${RESET}"
-cp -ru "$HOME/.zshrc" ~/hyprdots/zsh/.zshrc
+cp -r "$HOME/.zshrc" ~/hyprdots/zsh/.zshrc
